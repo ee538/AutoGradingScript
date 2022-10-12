2dc3ff72d0d023cc9469121bc88e8cffd23e44f7
tvrctkmytfln <tvrctkmytfln@outlook.com>
2022-10-12 06:21:29 -0700

    indent

--- coding_grades_total.py
+++ coding_grades_total.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import json
 
-
 def get_ok_num_perq(tresfile):
     file = open(tresfile, "r")
     f = file.read()
@@ -10,30 +9,29 @@ def get_ok_num_perq(tresfile):
     return sum(x != '' for x in lists)
 
 if __name__ == '__main__':
-	
-	total_coding_score = 0.0;
-	q_nums = []
-	full_score = {}
-	test_cases = {}
 
-	with open('coding_grader/questions.json', encoding='utf-8') as q:
-	    result = json.load(q)
-	    q_nums = result.get('q_nums')
-	    full_score = result.get('full_score')
-	    test_cases = result.get('test_cases')
+    total_coding_score = 0.0;
+    q_nums = []
+    full_score = {}
+    test_cases = {}
 
-	score_per_test = { i: (full_score[i] * 2 // test_cases[i]) / 2 for i in q_nums }
+    with open('coding_grader/questions.json', encoding='utf-8') as q:
+        result = json.load(q)
+        q_nums = result.get('q_nums')
+        full_score = result.get('full_score')
+        test_cases = result.get('test_cases')
 
-	for q_num in q_nums:
-		pass_num = get_ok_num_perq("grades/Q" + q_num + "res_.txt")
-		if pass_num < test_cases[q_num]:
-			score = pass_num * score_per_test[q_num]
-		else:
-			score = full_score[q_num]
-		print("Q",q_num,": ", pass_num, "/", test_cases[q_num], "passed | score:", score)
-		total_coding_score += score
+    score_per_test = { i: (full_score[i] * 2 // test_cases[i]) / 2 for i in q_nums }
 
+    for q_num in q_nums:
+        pass_num = get_ok_num_perq("grades/Q" + q_num + "res_.txt")
+        if pass_num < test_cases[q_num]:
+            score = pass_num * score_per_test[q_num]
+        else:
+            score = full_score[q_num]
+        print("Q",q_num,": ", pass_num, "/", test_cases[q_num], "passed | score:", score)
+        total_coding_score += score
 
-	print("-----------------------------------------")
-	print("Your total score of coding section:", total_coding_score)
-	print("-----------------------------------------")
+    print("-----------------------------------------")
+    print("Your total score of coding section:", total_coding_score)
+    print("-----------------------------------------")
